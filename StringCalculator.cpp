9a9d6d784625258f2e58992036275cb86d11262b
Harish-C-15 <harish1ga18cs060@gmail.com>
2024-09-13 16:58:53 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -6,12 +6,26 @@
 #include <vector>
 #include <stdexcept>
 
+std::vector<int> negatives;
+int flag = 0;
 class NegativeNumberException : public std::runtime_error {
 public:
     NegativeNumberException(const std::string& message)
         : std::runtime_error(message) {}
 };
-
+void throw_error()
+{
+        std::string message = "Negatives not allowed: ";
+        for (int i = 0; i < negatives.size(); ++i) 
+            {
+                message += std::to_string(negatives[i]);
+                if (i < negatives.size() - 1) 
+                {
+                    message += ", ";
+                }
+            }
+        throw NegativeNumberException(message);
+}
 int StringCalculator::less_than_thousand(int num)
     {
        if (num>1000)
@@ -19,7 +33,20 @@ int StringCalculator::less_than_thousand(int num)
       else
          return num;
     }
-    int StringCalculator::add(std::string input)
+int get_number(ch token)
+{
+   int num = std::stoi(token);
+    if(num>0)
+        int digit = less_than_thousand(num);
+    else
+    {
+        negatives.push_back(num);
+        flag++;
+    }
+    return digit;
+}
+    
+int StringCalculator::add(std::string input)
    { 
        if (input.substr(0, 2) == "//") 
      {
@@ -30,28 +57,14 @@ int StringCalculator::less_than_thousand(int num)
       std::replace(input.begin(), input.end(), '\n', ',');
       int sum = 0;
       std::stringstream ss(input);
-      std::string token; 
-      std::vector<int> negatives;
-       
+      std::string token;  
       while(std::getline(ss,token,','))
       {
-          int num = std::stoi(token);
-          if(num>0)
-              sum = sum + less_than_thousand(num);
-          else
-          {
-              negatives.push_back(num);
-          }
-      }
-       if (!negatives.empty()) {
-        std::string message = "Negatives not allowed: ";
-        for (int i = 0; i < negatives.size(); ++i) {
-            message += std::to_string(negatives[i]);
-            if (i < negatives.size() - 1) {
-                message += ", ";
-            }
-        }
-        throw NegativeNumberException(message);
+          int num = get_number(token);
+          int sum = 0;
+          sum += num;
     }
+       if(flag>0)
+          throw_error();
       return sum;
    }
