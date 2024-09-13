c9a202b53a4a6c2be2d2700a8ac9c9588253e617
Harish-C-15 <harish1ga18cs060@gmail.com>
2024-09-13 15:18:39 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,1 +1,30 @@
+#include "StringCalculator.h"
+#include <sstream>
+#include <iostream>
+#include <cctype>
 
+int StringCalculator::less_than_thousand(int num)
+    {
+       if (num>1000)
+         return 0;
+      else
+         return num;
+    }
+    int StringCalculator::get_number(char ch)
+   {
+      if(isdigit(ch))
+      {
+        return less_than_thousand(ch);
+      }
+     return 0;
+   }
+    int StringCalculator::add(std::string input)
+   { 
+      int sum = 0;
+      for( char ch : input)
+        {
+          int n = get_number(ch);
+          sum += n;
+        }
+      return sum;
+   }
