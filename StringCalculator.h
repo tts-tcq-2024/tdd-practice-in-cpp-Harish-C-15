12e18267023a2433e7ae176ab68e62fb66bf57ea
Harish-C-15 <harish1ga18cs060@gmail.com>
2024-09-13 15:19:09 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -8,30 +8,8 @@
 class StringCalculator
 {
   public:
-    int less_than_thousand(int num)
-    {
-       if (num>1000)
-         return 0;
-      else
-         return num;
-    }
-    int get_number(char ch)
-   {
-      if(isdigit(ch))
-      {
-        return less_than_thousand(ch);
-      }
-     return 0;
-   }
-    int add(std::string input)
-   { 
-      int sum = 0;
-      for( char ch : input)
-        {
-          int n = get_number(ch);
-          sum += n;
-        }
-      return sum;
-   }
+    int less_than_thousand(int num);
+    int get_number(char ch);
+    int add(std::string input);
 };
 #endif // StringCalculator_H
