c974c54b599dd465dc31d2b256f349f148889964
Harish-C-15 <harish1ga18cs060@gmail.com>
2024-09-16 09:08:22 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -36,6 +36,14 @@ TEST(StringCalculatorAddTests, ExpectExceptionForNegativeNumbers) {
         }, std::runtime_error);
 }
 
+TEST(StringCalculatorAddTests, ExpectExceptionForMultipleNegativeNumbers) {
+    ASSERT_THROW({
+        std::string input = "-1,-2";
+        StringCalculator objUnderTest;
+       objUnderTest.add(input);
+        }, std::runtime_error);
+}
+
 TEST(StringCalculatorAddTests, ExpectSumWithNewlineDelimiter) {
     int expectedresult = 6;
     std::string input = "1\n2,3";
@@ -57,6 +65,15 @@ TEST(StringCalculatorAddTests, IgnoreNumbersGreaterThan1000) {
 TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter) {
     int expectedresult = 3;
     std::string input = "//;\n1;2";
+    StringCalculator objUnderTest;
+    int result = objUnderTest.add(input);
+
+    ASSERT_EQ(result, expectedresult);
+}
+
+TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter) {
+    int expectedresult = 3;
+    std::string input = "//;\n1;;2";
     StringCalculator objUnderTest;
     int result = objUnderTest.add(input);
 
