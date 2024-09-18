90711cb8b564590fe8a2c7e204914d9d9c5d7aad
Harish-C-15 <harish1ga18cs060@gmail.com>
2024-09-18 15:23:45 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -8,9 +8,9 @@
 class StringCalculator
 {
   public:
-    int less_than_thousand(int num);
+    int check_for_less_than_thousand(int num);
     int get_positive_number(int num);
-    std::string processed_String_for_delimiters(std::string input);
+    std::string Process_String_for_delimiters(std::string input);
     int exception_handling_for_whitespce(std::string token);
     int add(std::string input);
 };
